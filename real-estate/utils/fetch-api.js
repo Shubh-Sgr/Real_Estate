44aad33af26c4624c9f6c9e293cfd944599ddc12
Shubham Sagar <dsacse384@gmail.com>
2021-12-24 10:22:25 +0530

    some changes

--- real-estate/utils/fetch-api.js
+++ real-estate/utils/fetch-api.js
@@ -6,13 +6,10 @@ export const fetchPropertyData = async (url) => {
   const response = await axios.get((url), {
     headers: {
         'x-rapidapi-host': 'bayut.p.rapidapi.com',
-        'x-rapidapi-key': 'd11adc3198msh1f0cc3fc6b12742p1d919fjsn0ac366a3736d'
+        'x-rapidapi-key': "your-api-key"
       }
   });
   return response.data;
 };
 
-// headers: {
-//     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-//     'x-rapidapi-key': 'd11adc3198msh1f0cc3fc6b12742p1d919fjsn0ac366a3736d'
-//   }
+
